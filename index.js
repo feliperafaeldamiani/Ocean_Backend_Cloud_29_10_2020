3ac677842a83976e22d073f77380f6d9d5d38c26
FD <feliperafael.damiani@gmail.com>
2020-10-29 16:44:16 -0300

    Conexão com o MongoDB Atlas

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const ObjectId = mongodb.ObjectId;
 
 (async () => {
 
-const connectionString = 'mongodb://localhost:27017';
+const connectionString = 'mongodb+srv://admin:admin@cluster0.n5dos.mongodb.net/ocean_MongoDB?retryWrites=true&w=majority';
+//const connectionString = 'mongodb+srv://admin:uoR24bocF7WUEVBT@cluster0.qvb7c.mongodb.net/ocean_MongoDB?retryWrites=true&w=majority';
+//const connectionString = 'mongodb://localhost:27017';
 
 console.info('Conectando ao banco de dados MongoDB...');
 
@@ -13,7 +15,7 @@ const options = {
     useUnifiedTopology: true
 };
 
-//const client = await mongodb.MongoClient.connect(connectionString, options);
+const client = await mongodb.MongoClient.connect(connectionString, options);
 
 console.info('MongoDB conectado com sucesso!');
 
@@ -58,9 +60,9 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
-//const db = client.db('ocean_backend_27_10_2020');
-//const mensagens = db.collection('mensagens');
-/*
+const db = client.db('ocean_MongoDB');
+const mensagens = db.collection('mensagens');
+
 // Read all
 app.get('/mensagem', async function (req, res) {
     const findResult = await mensagens.find().toArray();
@@ -117,7 +119,7 @@ app.delete('/mensagem/:id', async function (req, res) {
 
     res.send(`A mensagem de ID ${id} foi removida com sucesso.`);
 });
-*/
+
 app.listen(port, function () {
     console.info('App rodando em http://localhost:' + port);
 });
